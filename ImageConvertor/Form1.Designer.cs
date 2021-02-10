e4300ea488c26af35856aa471b37240d3cdac712
neytiri32 <mijocevic.matea@gmail.com>
2021-02-10 11:51:40 +0100

    existing project

---
+++ ImageConvertor/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+namespace ImageConvertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.SaveAsButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(30, 69);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(121, 31);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // SaveAsButton
+            // 
+            this.SaveAsButton.Location = new System.Drawing.Point(30, 311);
+            this.SaveAsButton.Name = "SaveAsButton";
+            this.SaveAsButton.Size = new System.Drawing.Size(121, 31);
+            this.SaveAsButton.TabIndex = 1;
+            this.SaveAsButton.Text = "Save As...";
+            this.SaveAsButton.UseVisualStyleBackColor = true;
+            this.SaveAsButton.Click += new System.EventHandler(this.SaveAsButton_Click);
+            // 
+            // comboBox1
+            // 
+            string[] imageFormat1 = new string[] {"*.bmp", "*.gif", "*.tiff", "*.jpg", "*.jpeg"};
+            this.comboBox1.DataSource = imageFormat1;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            string[] imageFormat2 = new string[] { "*.bmp", "*.gif", "*.tiff", "*.jpg", "*.jpeg" };
+            this.comboBox2.DataSource = imageFormat2;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(30, 281);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(166, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 303);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(526, 381);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.SaveAsButton);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button OpenButton;
+        private Button SaveAsButton;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ ImageConvertor/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageConvertor
+{
+    public partial class Form1 : Form
+    {
+
+        private Bitmap m_bitmap = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap ResizeBitmap(Bitmap img)
+        {
+            int newWidth = 0;
+            int newHeight = 0;
+            double imgRatio;
+
+            if (img.Width > img.Height)
+            {
+                imgRatio = ((double)img.Height / (double)img.Width) * 100;
+                newWidth = pictureBox1.Width;
+                newHeight = (int)(((double)newWidth / 100) * imgRatio);
+            }
+            else
+            {
+                imgRatio = ((double)img.Width / (double)img.Height) * 100;
+                newHeight = pictureBox1.Height;
+                newWidth = (int)(((double)newHeight / 100) * imgRatio);
+            }
+
+            Bitmap newImg = new Bitmap(newWidth, newHeight);
+
+            using (Graphics g = Graphics.FromImage(newImg))
+                g.DrawImage(img, 0, 0, newWidth, newHeight);
+
+            return newImg;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open image...";
+            ofd.Filter = comboBox1.Text + "|" + comboBox1.Text;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                m_bitmap = new Bitmap(ofd.FileName, true);
+                Bitmap resized = ResizeBitmap(m_bitmap);
+                pictureBox1.Image = resized;
+            }    
+        }
+
+        private void SaveAsButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save image as...";
+            sfd.Filter = comboBox2.Text + "|" + comboBox2.Text;
+
+            if (m_bitmap == null)
+            {
+                MessageBox.Show("You first need to upload image.", 
+                "Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning);
+            }
+            else if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                m_bitmap.Save(sfd.FileName);
+                MessageBox.Show("Picture saved",
+                "Info",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            }
+        }
+    }
+}
